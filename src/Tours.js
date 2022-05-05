6fa8e05a7a9d7ea4351697bdf6332cc6d502e45a
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-05-05 07:26:55 +0200

    fetch data using async and aawait

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Tours = () => {
-    return <h1>Our Tours</h1>
+const Tours = (tours) => {
+    return (
+        <div className="container">
+            <div className="title">
+                <h1 className="text-center display-1">My Tours</h1>
+                <div className="underline mt-4"></div>
+            </div>
+            
+            
+        </div>
+    );
 };
 
 export default Tours;
